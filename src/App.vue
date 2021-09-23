0ed88856dfdb233cead4baf6b18babb8188cf856
Thanh Thuy <lngththuy@gmail.com>
2021-09-23 22:42:06 +0700

    Upload source code

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>{{ test }}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      test: 'heelo',
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
